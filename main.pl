9bd8fd41a270434f2dec403703c0ce0c2f299444
Pedro Fonseca <pedrojgfonseca@gmail.com>
2022-12-12 16:45:21 +0000

    Move marble made

--- main.pl
+++ main.pl
@@ -36,10 +36,20 @@ valid_move(Board, Lidx_i-Cidx_i, Lidx_f-Cidx_i):- Lidx_f > Lidx_i,
                                                   NewX is Lidx_f-1,
                                                   valid_move(Board, Lidx_i-Cidx_i, NewX-Cidx_i).
 
-all_valid_moves(Moves):- start_board(C),
-                         valid_move(C, 3-3, 2-3).
+marble_naming(Player, Marble, Board, Target) :- atom_chars(Player, PlayerStr),
+                                                append("_", Marble, Suffix),
+                                                append(PlayerStr, Suffix, Target).
 
-get_marble_position(Player, Marble, Board, Line-Column):- atom_chars(Player, PlayerStr),
-                                                          append("_", Marble, Suffix),
-                                                          append(PlayerStr, Suffix, Target),
+get_marble_position(Player, Marble, Board, Line-Column):- marble_naming(Player, Marble, Board, Target),
                                                           findIndexesBoard(Board, Target, Line-Column).
+
+move_marble(Player, Marble, Board, Line-Column, NewBoard):-     get_marble_position(Player, Marble, Board, L-C),
+                                                                valid_move(Board, L-C, Line-Column),
+                                                                nth0(Line, Board, BoardLine),
+                                                                nth0(Column, BoardLine, _, [Prev, Post]),
+                                                                marble_naming(Player, Marble, Board, Target),
+                                                                NewBoard is [Prev, Target, Post],
+                                                                nth0(L, NewBoard, BoardLine),
+                                                                nth0(C, BoardLine, _, [Prev, Post]),
+                                                                NewBoard is [Prev, 'empty', Post].
+                                        
