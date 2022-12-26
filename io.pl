ed8f99c51007f6481c97ce2cf14725ee91bde3cb
Andre Sousa <up202005277@up.pt>
2022-12-26 11:19:52 +0000

    some comments

--- io.pl
+++ io.pl
@@ -1,3 +1,6 @@
+% print_line(+List:list(atom))
+% Prints each atom in the list with a fixed amount of padding on both sides, separated by a space.
+% Calls nl/0 at the end to print a new line.
 print_line([]):- nl.
 print_line([block|Rest]):- print_formatted('     ', 11),
                            print_line(Rest).
@@ -5,6 +8,9 @@ print_line([block|Rest]):- print_formatted('     ', 11),
 print_line([First|Rest]):- print_formatted(First, 11),
                            print_line(Rest).
 
+% display_game(+Board:list(list(atom)), +LineIndex:integer)
+% Prints the game board represented by the list of lists, with row and column indexes.
+% The integer represents the current row index.
 display_game([], _):-nl.
 display_game([FirstLine|Rest], LineIndex):- append([LineIndex], FirstLine, Tmp),
                                            append(Tmp, [LineIndex], Res),
@@ -12,7 +18,8 @@ display_game([FirstLine|Rest], LineIndex):- append([LineIndex], FirstLine, Tmp),
                                            nl,
                                            NewIdx is LineIndex+1,
                                            display_game(Rest, NewIdx).
-
+% display_game(+Board:list(list(atom)))
+% Prints the game board represented by the list of lists, with row and column indexes.
 display_game(Board):- size(Size),
                      Max is Size-1,
                      numlist(0, Max, L),
@@ -21,16 +28,22 @@ display_game(Board):- size(Size),
                      display_game(Board, 0),
                      print_line(Indexes).
 
+% print_n(+Atom:atom, +Count:integer)
+% Prints the atom the specified number of times.
 print_n(_, 0).
 print_n(S, N):- N>0,
                 write(S),
                 N1 is N-1,
                 print_n(S, N1).
 
+% print_text(+Atom:atom, +Padding:integer)
+% Prints the atom with a fixed amount of padding on both sides, such that the total length of the printed string is equal to the integer.
 print_text(TextAtom, Padding):- print_n(' ', Padding),
                                 write(TextAtom),
                                 print_n(' ', Padding).
 
+% print_formatted(+Content:integer|atom, +TargetSize:integer)
+% Prints the atom with a fixed amount of padding on both sides, such that the total length of the printed string is equal to the integer.
 print_formatted(Content, TargetSize):- \+atom(Content),
                             number_codes(Content, Codes),
                             atom_codes(Atom, Codes),
@@ -41,12 +54,21 @@ print_formatted(Content, TargetSize):- atom_length(Content, ContentLength),
                                          Remaining >= 0,
                                          print_text(Content, Remaining).
 
+% check_marble_id(+Marble:integer)
+% Checks if the integer is a valid marble ID (between 1 and 8, inclusive).
+% If the ID is invalid, prints an error message and fails.
 check_marble_id(Marble):- Marble > 0, Marble < 9, peek_char('\n'), get_char('\n').
 check_marble_id(_):- write('Invalid marble! Your choice must be between 1 and 8, including.\n'), clear_input, fail.
 
+% Checks if the current character in the input stream is a hyphen ('-').
+% If the character is not a hyphen, prints an error message, clears the input stream and fails.
 check_separator:- get_char('-').
 check_separator:- write('The coordinates input must be in the format "LineIndex-ColumnIndex".\nFurthermore, each coordinate must be between 0 and 8, including.\n'), !, clear_input, fail.
 
+% retrieve_command(+Player:atom, -Marble:integer, -Coordinates:pair(integer))
+% Prompts the user for the ID of a marble and the destination coordinates (line and column).
+% Reads the input from the standard input stream.
+% Validates the input and fails if it is invalid.
 retrieve_command(Player, Marble, Line-Column):- write(Player), write(' it is your turn!'),
                                                 nl,
                          write('Which marble you want to move? '),
@@ -58,12 +80,25 @@ retrieve_command(Player, Marble, Line-Column):- write(Player), write(' it is you
                          read_number(Column),
                          get_code(10).
 
+% check_option(+Option:integer, -GameMode:atom)
+% Checks if the integer is a valid game mode option (1, 2, 3 or 4).
+% If the option is valid, binds the atom to the corresponding game mode.
+% If the option is invalid, prints an error message and fails.
 check_option(Option, GameMode):- gamemode(Option, GameMode).
 check_option(_, _):-  write('Invalid Option! Your choice must be one of 1, 2, 3 or 4: '), fail.
 
+% check_option_ai_level(+Option:integer, -Level:atom)
+% Checks if the integer is a valid AI level option (1 or 2).
+% If the option is valid, binds the atom to the corresponding AI level.
+% If the option is invalid, prints an error message and fails.
 check_option_ai_level(Option, Level):- ai_level(Option, Level).
 check_option_ai_level(_, _):- write('Invalid Option! Your choice must be one of 1 or 2: '), fail.
 
+% start_menu(-GameMode:atom)
+% Prompts the user to select a game mode from a list of options.
+% Reads the input from the standard input stream.
+% Validates the input and fails if it is invalid.
+% Binds the atom to the selected game mode.
 start_menu(GameMode):- write('Greetings! This is our implementation of the Wana game.\n'),
                          write('We offer you different game modes, namely:\n'),
                          write('1- Multiplayer mode (human vs human)\n'),
@@ -76,6 +111,10 @@ start_menu(GameMode):- write('Greetings! This is our implementation of the Wana 
                          get_code(10),
                          check_option(Option, GameMode).
 
+% set_ai_level(-Level:atom)
+% Prompts the user to select an AI level from a list of options.
+% Reads the input from the standard input stream.
+% Validates the input and fails
 set_ai_level(Level):- write('In our game you can choose one of 2 levels:\n'),
                  write('1- Easy\n'),
                  write('2- Hard\n'),
--- main.pl
+++ main.pl
@@ -1,7 +1,9 @@
 :-use_module(library(lists)), use_module(library(random)), use_module(library(between)).
 :-dynamic bot_difficulty/2.
-
 :- ['io.pl', 'utils.pl', 'ai.pl', 'valid_moves.pl'].
+
+% start_board(-Board:list(list(atom))) is det
+% Initializes the game board as a list of lists of atoms.
 start_board([
         [block, block, block, player2_1, empty, player2_2, block, block, block],
         [block, block, block, player2_3, empty, player2_4, block, block, block],
@@ -14,32 +16,58 @@ start_board([
         [block, block, block, player1_1, empty, player1_2, block, block, block]
         ]).
 
+% size(-Size:integer) is det
+% Initializes the size of the game board as an integer.
 size(9).
 
+% player(-Player:atom) is nondet
+% Yields the atoms representing the players.
 player(player1).
 player(player2).
 
+% marbles(+Player:atom, -Marbles:list(atom)) is det
+% Returns a list of atoms representing the marbles of a player.
 marbles(player1, [player1_1, player1_2, player1_3, player1_4, player1_5, player1_6, player1_7, player1_8]).
 marbles(player2, [player2_1, player2_2, player2_3, player2_4, player2_5, player2_6, player2_7, player2_8]).
 
+% gamemode(+Option:integer, -GameMode:atom) is semidet
+% Validates an integer as a game mode option (1, 2, 3, or 4).
+% Returns the corresponding game mode atom.
+% Fails if the option is invalid.
 gamemode(1, hVh).
 gamemode(2, hVc).
 gamemode(3, cVh).
 gamemode(4, cVc).
 
+% ai_level(+Option:integer, -Level:atom) is semidet
+% Validates an integer as an AI level option (1 or 2).
+% Returns the corresponding level atom.
+% Fails if the option is invalid.
 ai_level(1, easy).
 ai_level(2, hard).
 
+% next_player(+Player:atom, -NextPlayer:atom) is det
+% Returns the next player in the game based on the current player.
 next_player(player1, player2).
 next_player(player2, player1).
 
+% get_all_moves_from_pos(+Board:list(list(atom)), +Coordinates:pair(integer), -Moves:list(pair(integer))) is det
+% Returns a list of valid moves for a marble at a given position on the board.
+% The board is represented as a list of lists of atoms, and the position is given
 get_all_moves_from_pos(Board, Lidx_i-Cidx_i, Moves):- findall(L-C, valid_move(Board, Lidx_i-Cidx_i, L-C), Res), 
                                              remove_dups(Res, Deduplicated),
                                              list_del(Deduplicated, Lidx_i-Cidx_i, Moves).
 
+% get_marble_position(+Player:atom, +Marble:atom, +Board:list(list(atom)), -Coordinates:pair(integer)) is det
+% Returns the coordinates of a marble on the board.
+% The board is represented as a list of lists of atoms, the player is an atom, and the marble is an atom.
 get_marble_position(Player, Marble, Board, Line-Column):- marble_naming(Player, Marble, Res),
                                                           findIndexesBoard(Board, Res, Line-Column).
 
+% move(+Player:atom, +Marble:atom, +Board:list(list(atom)), +Coordinates:pair(integer), -NewBoard:list(list(atom))) is det
+% Makes a move on the board for a given player and marble, and returns the resulting board.
+% The board is represented as a list of lists of atoms, the player is an atom, the marble is an atom, and the coordinates are given as a pair of integers.
+% Fails if the move is not valid.
 move(Player, Marble, Board, Line-Column, NewBoard):-     atom_string(Player, PlayerString),
                                                                 atom_string_number(Marble, MarbleString),
                                                                 get_marble_position(PlayerString, MarbleString, Board, L-C),
@@ -53,11 +81,18 @@ get_all_positions(Board, [FirstMarble|Rest], Positions):- findIndexesBoard(Board
                                                           get_all_positions(Board, Rest, Res),
                                                           append([L-C], Res, Positions).
 
+% check_all_moves(+Board:list(list(atom)), +Positions:list(pair(integer))) is det
+% Validates that all marbles have at least one valid move.
+% The board is represented as a list of lists of atoms, and the positions are given as a list of pairs of integers.
+% Fails if at least one marble has no valid moves.
 check_all_moves(_, []).
 check_all_moves(Board, [L-C|Tail]):- get_all_moves_from_pos(Board, L-C, Res),
                                      \+length(Res, 0),
                                      check_all_moves(Board, Tail).
 
+% make_move(+Player:atom, +Board:list(list(atom)), -NewBoard:list(list(atom))) is det
+% Makes a move for a given player and returns the resulting board.
+% The board is represented as a list of lists of atoms, and the player is an atom.
 make_move(Player, Board, NewBoard):- retrieve_command(Player, Marble, LineDest-ColumnDest),
                                      atom_string(Player, PlayerString),
                                      atom_string_number(Marble, MarbleString),
@@ -65,11 +100,15 @@ make_move(Player, Board, NewBoard):- retrieve_command(Player, Marble, LineDest-C
                                      check_valid_move(Board, L-C, LineDest-ColumnDest),
                                      move(Player, Marble, Board, LineDest-ColumnDest, NewBoard).
 
+% make_move_ai(+Difficulty:atom, +Player:atom, +Board:list(list(atom)), -NewBoard:list(list(atom))) is det
+% Makes a move for an AI player and returns the resulting board.
+% The difficulty level is an atom, the board is represented as a list of lists of atoms, and the player is an atom.
 make_move_ai(Difficulty, Player, Board, NewBoard):- best_move_ai(Difficulty, Board, Player, Marble, LineMove-ColumnMove),
                                               findIndexesBoard(Board, Marble, Line-Column),
                                               insert_in_board(Board, Marble, LineMove, ColumnMove, NB1),
                                               insert_in_board(NB1, empty, Line, Column, NewBoard).
 
+% Main entry point of the game. Initializes the board, displays it, and starts the game with a selected game mode.
 play:- start_board(Board),
             player(Player),
             display_game(Board),
@@ -77,6 +116,9 @@ play:- start_board(Board),
             !,
             game_cycle_wrapper(GameMode, Board-Player).
 
+% game_cycle_wrapper(+GameMode:atom, +Board-Player:pair(list(list(atom)), atom)) is det
+% Wrapper function to handle the different game modes. Initializes AI difficulty level if needed.
+% The game mode is an atom and the board and current player are represented as a pair (list of lists of atoms and atom).
 game_cycle_wrapper(hVh, Board-Player):- game_cycle(hVh, Board-Player).
 game_cycle_wrapper(cVh, Board-Player):- set_ai_level(Level),
                                         assert(bot_difficulty(player1, Level)),
@@ -92,10 +134,16 @@ game_cycle_wrapper(cVc, Board-Player):- set_ai_level(Level1),
                                         assert(bot_difficulty(player2, Level2)),
                                         game_cycle(cVc, Board-Player).
 
+% game_over(+Board:list(list(atom)), +Player:atom) is det
+% Determines if the game is over for the given player.
+% The board is represented as a list of lists of atoms and the player is an atom.
 game_over(Board, Player):- marbles(Player, MarblesNames),
                            get_all_positions(Board, MarblesNames, Positions),
                            \+check_all_moves(Board, Positions).
 
+% game_cycle(+GameMode:atom, +Board-Player:pair(list(list(atom)), atom)) is det
+% Main game loop for the different game modes.
+% The game mode is an atom and the board and current player are represented as a pair (list of lists of atoms and atom).
 game_cycle(_, Board-Player):-game_over(Board, Player), !,
                            next_player(Player, NextPlayer),
                            congratulate(NextPlayer),
